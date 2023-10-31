a840ea2b641e2512f610ac16c7c9f335f58e2655
luijika711 <luijika711@gmail.com>
2023-10-31 20:55:47 +0200

    2023 judeteana added, I forgot how garbage SQL is, Ill go play fortnite now

---
+++ judeteana_2023/SV_ID_LOL/JocEducativ/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+namespace JocEducativ
+{
+    public partial class Form1 : Form
+    {
+        public void LoadDatabase()
+        {
+            using (SqlConnection Conn = new SqlConnection(Program.ConnectionString))
+            {
+                Conn.Open();
+                foreach(var Line in File.ReadAllLines("Utilizatori.txt"))
+                {
+                    var Cols = Line.Split(';');
+                    string QueryString = string.Format("SELECT * FROM Utilizatori WHERE EmailUtilizator = '{0}'", Cols[0].ToString());
+                    SqlCommand Cmd = new SqlCommand(QueryString, Conn);
+                    //SqlDataReader Reader = Cmd.ExecuteReader();
+                    Cmd = new SqlCommand("INSERT INTO Utilizatori VALUES('aoleu','aoleu','aoleu')",Conn);
+                    Cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDatabase();
+        }
+    }
+}
---
+++ judeteana_2023/SV_ID_LOL/JocEducativ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JocEducativ
+{
+    static class Program
+    {
+        public static string ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\bin\\Debug\\JocEducativ.mdf;Integrated Security=True;Connect Timeout=30";
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
